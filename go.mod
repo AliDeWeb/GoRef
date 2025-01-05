c097b0fda7a750ed917b64e6103e53c4dc0873cb
AliDeWeb <alimoradi0business@gmail.com>
2025-01-05 15:42:46 +0330

    add module and useful commands

---
+++ go.mod
@@ -0,0 +1,7 @@
+// This file is module
+//+ It includes the go version we use and dependencies versions
+//* Every Go app must have at least one .mod(module) file
+
+module github.com/alideweb/GoRef
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,24 @@
+/*
+	<-- Go Useful Commands -->
+
+	*** go mod init example.com/repo_name
+	// this command is used for creating go module with repo_name as name and example.com as namespace
+
+	*** go mod tidy
+	// this command is used for managing app dependencies,
+	when we run it, it would scan the whole code to see which dependencies are missed,
+	and then it would install them
+
+	*** go build .
+	// this command is used for build the app and compile it to an executable(.exe) file
+
+	*** ./build.exe
+	// after the building process is over, we can use the command above to run our compiled app
+
+	*** go run .
+	// this command is used for run our app without compiling it, it is good for development
+*/
+
+package main
+
+func main() {}
